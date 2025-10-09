4799fdd9aa164e27a5e3970a420fa82611af38db
monty <jinmontykang22@tamu.edu>
2025-10-08 20:06:30 -0500

    Update dbSetup.java

--- javafx_demo_GUI/src/jdbc_scenebuilder/dbSetup.java
+++ javafx_demo_GUI/src/jdbc_scenebuilder/dbSetup.java
@@ -5,6 +5,6 @@ Database setup example
 Purpose: Keeping usernames and password private.
 */
 public final class dbSetup  {
-  public static final String user = "gang_52";
-  public static final String pswd = "miMima";
+  public static final String user = "NONE"; //changed spec for security
+  public static final String pswd = "NONE";
 }//end class
